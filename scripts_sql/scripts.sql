a78a1201845adb0c974772302990f747e88f47c4
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-02-02 16:38:13 -0300

    Criando script para criação do banco de dados.

---
+++ scripts_sql/scripts.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS shortener_db;
+
+CREATE DATABASE shortener_db;
+
+USE shortener_db;
+
+CREATE TABLE tb_user (
+	login VARCHAR(20) PRIMARY KEY,
+	senha VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE tb_link (
+	curto VARCHAR(12) PRIMARY KEY,
+	original VARCHAR(256) NOT NULL,
+	user_login VARCHAR(20),
+	FOREIGN KEY (user_login) REFERENCES tb_user(login)
+);
+
+CREATE TABLE tb_acess (
+	link VARCHAR(12),
+	endereco_ip VARCHAR(15),
+	FOREIGN KEY (link) REFERENCES tb_link(curto)
+);
