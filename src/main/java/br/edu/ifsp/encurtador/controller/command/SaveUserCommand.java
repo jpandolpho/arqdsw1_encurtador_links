a8d0b848c96a5f97ab08a95b84d98c3426061d69
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-02-03 09:37:09 -0300

    Adicionando lógica básica para o command de salvar usuário.

--- src/main/java/br/edu/ifsp/encurtador/controller/command/SaveUserCommand.java
+++ src/main/java/br/edu/ifsp/encurtador/controller/command/SaveUserCommand.java
@@ -2,6 +2,8 @@ package br.edu.ifsp.encurtador.controller.command;
 
 import java.io.IOException;
 
+import br.edu.ifsp.encurtador.model.dao.UserDaoFactory;
+import br.edu.ifsp.encurtador.model.entity.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,8 +13,20 @@ public class SaveUserCommand implements Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		var login = request.getParameter("textLogin");
+		var senha = request.getParameter("textPassword");
+		
+		var dao = new UserDaoFactory().factory();
+		var user = new User(login, senha);
+		var saved = dao.insert(user);
+		
+		if(saved) {
+			System.out.println("salvou usuário");
+		}else {
+			System.out.println("não salvou");
+		}
+		
+		return "front.do?action=home";
 	}
 
 }
