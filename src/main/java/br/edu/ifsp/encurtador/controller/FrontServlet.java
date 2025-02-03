1caafb50397ae81e58e77ff1e902892d4d9e867a
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-02-03 07:59:01 -0300

    Criando index e alguns includes. Criando versão inicial do FrontServlet.

---
+++ src/main/java/br/edu/ifsp/encurtador/controller/FrontServlet.java
@@ -0,0 +1,27 @@
+package br.edu.ifsp.encurtador.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/front.do")
+public class FrontServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request,response);
+	}
+	
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<jsp:include page="/includes/head.html"/>
+<body>
+	<jsp:include page="/includes/navbar.jsp"/>
+	<hr>
+	<h1>teste</h1>
+</body>
+</html>
