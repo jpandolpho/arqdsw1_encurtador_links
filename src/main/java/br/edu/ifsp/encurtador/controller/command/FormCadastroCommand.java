07be5ddfc40ef39cea3c02d34afbb90d00ab9a09
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-02-03 09:05:52 -0300

    Criando servlet para redirecionar para formulário de cadastro.

---
+++ src/main/java/br/edu/ifsp/encurtador/controller/command/FormCadastroCommand.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.encurtador.controller.command;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FormCadastroCommand implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		return "/formCadastro.jsp";
+	}
+
+}
